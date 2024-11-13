29ea232c4973bf64005e95fb39486d2b945b70db
KaungKhantSiThu <64033794+KaungKhantSiThu@users.noreply.github.com>
2024-11-13 14:10:04 +0700

    Initial Commit

---
+++ TMDB Client/TMDB_ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDB_ClientApp.swift
+//  TMDB Client
+//
+//  Created by Kaung Khant Si Thu on 13/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct TMDB_ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
